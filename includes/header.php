2f5292203f03346814559e2cce62820c48716f85
David Mendoza <davidmendozaofficial@gmail.com>
2019-03-19 01:56:44 +0800

    First Commit
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>Quiz App</title>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link rel="stylesheet" href="../css/bootstrap.min.css">
+		<link rel="stylesheet" href="../css/font-awesome.min.css">
+	</head>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include('includes/header.php'); ?>
+
+<body>
+	<?php include('includes/navbar.php'); ?>
+
+	<div class="container">
+		
+	</div>
+</body>
+
+<?php include('includes/scripts.php'); ?>
+<?php include('includes/footer.php'); ?>
